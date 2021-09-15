0eccea62c0192871299f96444a3418aea931bb3d
sebastianfori <you.com>
2021-09-15 07:59:03 -0300

    hecizoterminado

---
+++ src/Library/Hechizo.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+
+namespace Library
+{
+    public class Hechizo
+    {
+        public string Nombre{get; set private;}
+        public int Ataque {get; set private;}
+        public int Defensa {get ;set private;}
+        
+        public Hechizo(string Nombre ,int Ataque, int Defensa)
+        {
+            this.Nombre=pnombre;
+            this.Ataque=pataque;
+            this.Defensa=pdefensa;
+        }
+    }
+}
